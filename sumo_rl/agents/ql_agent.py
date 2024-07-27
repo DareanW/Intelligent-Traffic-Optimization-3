af5b0cd275e65ecc10ddcb270c98592efe052d70
Darean Wilde <darean.wilde@my.utsa.edu>
2024-07-27 08:27:49 -0500

    pretty much replacing the example learning method with the pTLC learning method.

--- sumo_rl/agents/ql_agent.py
+++ sumo_rl/agents/ql_agent.py
@@ -1,25 +1,30 @@
-"""Q-learning Agent class."""
 from sumo_rl.exploration.epsilon_greedy import EpsilonGreedy
-
+import numpy as np
 
 class QLAgent:
-    """Q-learning Agent class."""
+    """pTLC Agent class."""
 
-    def __init__(self, starting_state, state_space, action_space, alpha=0.5, gamma=0.95, exploration_strategy=EpsilonGreedy()):
-        """Initialize Q-learning agent."""
+    def __init__(self, starting_state, state_space, action_space, alpha=0.5, gamma=0.95, epsilon_start=0.9, epsilon_end=0.01, epsilon_decay=80000, exploration_strategy=EpsilonGreedy()):
+        """Initialize pTLC agent."""
         self.state = starting_state
         self.state_space = state_space
         self.action_space = action_space
-        self.action = None
         self.alpha = alpha
         self.gamma = gamma
+        self.epsilon = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_decay = epsilon_decay
         self.q_table = {self.state: [0 for _ in range(action_space.n)]}
-        self.exploration = exploration_strategy
+        self.exploration = EpsilonGreedy(epsilon_start, epsilon_end, epsilon_decay)
         self.acc_reward = 0
+        self.pduration = 0
 
     def act(self):
         """Choose action based on Q-table."""
-        self.action = self.exploration.choose(self.q_table, self.state, self.action_space)
+        if np.random.rand() < self.epsilon:
+            self.action = np.random.choice(self.action_space.n)
+        else:
+            self.action = np.argmax(self.q_table[self.state])
         return self.action
 
     def learn(self, next_state, reward, done=False):
@@ -30,8 +35,9 @@ class QLAgent:
         s = self.state
         s1 = next_state
         a = self.action
-        self.q_table[s][a] = self.q_table[s][a] + self.alpha * (
-            reward + self.gamma * max(self.q_table[s1]) - self.q_table[s][a]
-        )
+        best_next_action = np.argmax(self.q_table[s1])
+        td_target = reward + self.gamma * self.q_table[s1][best_next_action]
+        self.q_table[s][a] += self.alpha * (td_target - self.q_table[s][a])
         self.state = s1
         self.acc_reward += reward
+        self.epsilon = max(self.epsilon_end, self.epsilon * np.exp(-1.0 / self.epsilon_decay))
